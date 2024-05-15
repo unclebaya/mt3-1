4982618e39766c21ef9a8a133af5c936c6f8e0dd
Jake VanderPlas <vanderplas@google.com>
2024-05-14 19:18:09 -0700

    Replace deprecated `jax.tree_*` functions with `jax.tree.*`
    The top-level `jax.tree_*` aliases have long been deprecated, and will soon be removed. Alternate APIs are in `jax.tree_util`, with shorter aliases in the `jax.tree` submodule, added in JAX version 0.4.25.
    
    PiperOrigin-RevId: 633777708

--- mt3/layers_test.py
+++ mt3/layers_test.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The MT3 Authors.
+# Copyright 2024 The MT3 Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -500,7 +500,7 @@ class DenseTest(parameterized.TestCase):
         dtype=np.float32)
     params = module.init(random.PRNGKey(0), inputs, deterministic=True)
     self.assertEqual(
-        jax.tree_map(lambda a: a.tolist(), params), {
+        jax.tree.map(lambda a: a.tolist(), params), {
             'params': {
                 'wi': {
                     'kernel': [[
